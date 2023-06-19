ece606fdc293bfbed89e0f784088ba71a8faeebf
Revitall <RevitallAzizi@gmail.com>
2023-06-19 22:00:30 +0430

    MainActivity Finished

---
+++ app/src/main/java/ir/shariaty/mypaycard/GhabzActivity.java
@@ -0,0 +1,14 @@
+package ir.shariaty.mypaycard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GhabzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ghabz);
+    }
+}
--- app/src/main/java/ir/shariaty/mypaycard/MainActivity.java
+++ app/src/main/java/ir/shariaty/mypaycard/MainActivity.java
@@ -2,13 +2,42 @@ package ir.shariaty.mypaycard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgphone;
+    ImageView imgGhabz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+            imgphone=findViewById(R.id.img1);
+            imgGhabz=findViewById(R.id.img5);
+
+
+            imgphone.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intentmain=new Intent(MainActivity.this,CellPhoneActivity.class);
+                    startActivity(intentmain);
+
+                }
+            });
+
+            imgGhabz.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent=new Intent(MainActivity.this,GhabzActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+
+        }
     }
-}
